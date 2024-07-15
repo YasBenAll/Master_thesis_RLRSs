5fbbfa8e8e1ac523ffcd0b92b1146481e08ddfda
YasBenAll <yassinbenallal@hotmail.com>
2024-07-15 16:51:39 +0200

    Update gen_dataset.py

--- gen_dataset.py
+++ gen_dataset.py
@@ -5,94 +5,34 @@ import numpy as np
 import os
 import pickle
 import torch
-from momdp.momdp import MOMDP
-from momdp.policy import create_policy
-from momdp.evaluation import evaluate_momdp
 from pathlib import Path
 from sardine.wrappers import IdealState
 from sardine.policies import EpsilonGreedyOracle, EpsilonGreedyAntiOracle, RandomPolicy
 from collections import OrderedDict
 np.set_printoptions(precision=3)
 
-def get_parser(parents  = [], args = None):
-    parser = argparse.ArgumentParser(parents = parents, add_help=False)
-    parser.add_argument('--env_id', type=str, default='ml-100k-v0')
-    parser.add_argument('--seed', type=int, default=2024)
-    parser.add_argument('--lp', type=str, default='oracle')
-    parser.add_argument('--eps', type=float, default=0.0)
-    parser.add_argument("--n_users", type=int, default=10)
-    parser.add_argument("--data_dir", type=str, default="data/")
-    parser.add_argument("--dataset_type", type=str, default="dict")
-    parser.add_argument("--specialist-ratio", type=float, default=0.0)
-    return parser
+seed = 2023
+env_id = "ml-100k-v0"
+data_dir = "C:/Users/Yassi/Documents/GitHub/Master_thesis_RLRSs/data/datasets/"
+Path(data_dir).mkdir(parents=True, exist_ok=True)
+Path(data_dir + "embeddings/").mkdir(parents=True, exist_ok=True)
+lp = "antioracle"
+eps = 0.0
 
-def generate_dataset(env, seed, lp, eps, n_users, dataset_type):
+## Let's create the environment of our choice
+env = gym.make(env_id)
 
-    logging_policy = create_policy(lp=lp, epsilon=eps, env=env, seed=seed)
-    dataset = env.unwrapped.generate_dataset(n_users=n_users, policy=logging_policy, seed=seed, dataset_type=dataset_type)
-    return dataset
+## If you want to work with Fully observable state, add a wrapper to the environment
+env = IdealState(env)
 
-def create_policy(lp, epsilon, env, seed):
-    if lp == "oracle":
-        return EpsilonGreedyOracle(epsilon = epsilon, env = env, seed = seed)
-    elif lp == "antioracle":
-        return EpsilonGreedyAntiOracle(epsilon = epsilon, env = env, seed = seed)
-    elif lp == "random":
-        return RandomPolicy(env = env, seed = seed)
-    else:
-        raise ValueError("Unknown logging policy")
-
-def evaluate(env, seed):
-    observation, info = env.reset(seed = seed)
-    env.action_space.seed(2024)
-    cum_reward_list, cum_boredom_list = [], []
-
-    cum_reward = np.zeros(3)  # For engagement, diversity, and novelty
-    cum_boredom = 0
-    ep = 0
-
-
-    while ep < 100:
-        # print(ep)
-        action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
-        clicks = info["clicks"]
-        slate = info["slate"]
-        rewards = reward
-        cum_reward += rewards
-        cum_boredom += (1.0 if np.sum(info["bored"] == True) > 0 else 0.0)
-
-        if terminated or truncated:
-            cum_reward_list.append(cum_reward)
-            cum_boredom_list.append(cum_boredom)
-            cum_reward = 0
-            cum_boredom = 0
-            observation, info = env.reset()
-            ep += 1
-    # print(cum_reward_list)
-    print("Average return: ", np.mean(cum_reward_list, axis=0))
-
-if __name__ == "__main__":
-    parser = get_parser()
-    args = parser.parse_args()
-    Path(args.data_dir).mkdir(parents=True, exist_ok=True)
-    Path(args.data_dir + "datasets/").mkdir(parents=True, exist_ok=True)
-
-    env = mo_gym.make(f"sardine/{args.env_id}")
-    env = IdealState(env)
-
-    dataset = generate_dataset(env, args.seed, args.lp, args.eps, args.n_users, args.dataset_type)
-    print(dataset)
-    print(type(dataset))
-    # save ordered dict
-    path = os.path.join("data", "datasets", f"env_data_{args.env_id}_lp_{args.lp}_epsilon_{args.eps}_seed_{args.seed}_n_users_{args.n_users}")
-
-    if type(dataset) == OrderedDict:
-        with open(f'{path}.pkl', 'wb') as f:
-            pickle.dump(dataset, f)
-            print("Saved dataset to ", path)
-    else:
-        torch.save(dataset, path+"_sb3_replay.pt")
-        print("Saved dataset to ", path)
-
-    print("Done!")
+## Generate a dataset of 10 users with 50% random actions and 50% greedy actions
+if lp == "oracle":
+    logging_policy = EpsilonGreedyOracle(epsilon = eps, env = env, seed = seed)
+elif lp == "antioracle":
+    logging_policy = EpsilonGreedyAntiOracle(epsilon = eps, env = env, seed = seed)
+dataset = env.generate_dataset(n_users = 10, policy = logging_policy, seed = seed, dataset_type="sb3_replay")
+print(dataset.observations.keys())
+print(dataset.rewards.dtype)
+path = env_id + "_" + lp + "_epsilon" + str(eps) + "_seed" + str(seed) + ".pt"
+torch.save(dataset, data_dir + path)
+torch.save(env.item_embedd, data_dir + "embeddings/" + path)
