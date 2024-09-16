a67e552500d05b0b83594172f9a145d701ea5ad9
YasBenAll <yassinbenallal@hotmail.com>
2024-09-16 11:21:02 +0200

    DAS6

---
+++ example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SBATCH --job-name=test
+# SBATCH --time=72:00:00
+# SBATCH -N 1
+# SBATCH --ntasks-per-node=1
+# SBATCH --partition=defq
+# SBATCH -C <resource name, see below> # this line is usually not needed
+# SBATCH --gres=gpu:1
+
+# Load GPU drivers
+ module load cuda11.3/toolkit/11.3.1
+
+# Base directory for the experiment
+mkdir $HOME/experiments
+cd $HOME/experiments
+
+# Simple trick to create a unique directory for each run of the script
+echo $$
+mkdir o`echo $$
+cd o`echo $$`
+
+# Run the actual experiment.
+python /home/yal700/git/Master_thesis_RLRSs/test.py
