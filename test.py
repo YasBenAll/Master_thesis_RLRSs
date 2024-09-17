062cdd1248cc47d3e491dd77c7e65344b752d7ba
YasBenAll <yassinbenallal@hotmail.com>
2024-09-17 14:49:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import torch
 
-print("output python file")
-print('Hello, World!')
-print(torch.__version__)
-print(str(torch.cuda.is_available()))
+print("Output python file:")
+print(f"Torch version: {torch.__version__}")
+print(f"Is Cuda available: {str(torch.cuda.is_available())}")
 
 with open('/home/yal700/git/Master_thesis_RLRSs/test.txt', 'w') as f:
-    f.write('Hello, World!\n')
-    f.write(torch.__version__ + '\n')
-    f.write(str(torch.cuda.is_available()))
+    f.write(f"Torch version: {torch.__version__}")
+    f.write(f"Is Cuda available: {str(torch.cuda.is_available())}")
 
