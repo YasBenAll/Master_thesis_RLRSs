48de70276c546a3238d637eebe5ab72b6a8a6c47
YasBenAll <yassinbenallal@hotmail.com>
2024-09-17 13:04:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import torch
 
-print("test2")
-print(torch.__version__)
+print('Hello, World!\n')
+print(torch.__version__ + '\n')
+print(str(torch.cuda.is_available()))
+
 with open('/home/yal700/git/Master_thesis_RLRSs/test.txt', 'w') as f:
     f.write('Hello, World!\n')
     f.write(torch.__version__ + '\n')
