496f148b4475a19d75a1a16ff0714f2190e196e9
YasBenAll <yassinbenallal@hotmail.com>
2024-09-22 20:10:56 +0200

    Update pretrain_gems.sh

--- pretrain_gems.sh
+++ pretrain_gems.sh
@@ -38,6 +38,6 @@ mkdir o`echo $$`
 cd o`echo $$`
 
 # Run the actual experiment
-python /var/scratch/yal700/Master_thesis_RLRSs/pretrain_gems.py --slate-size $SLATESIZE --latent-dim $LD --n-users 100000 --device cuda --multi True --seed 2023 --concurrent True --env-id SlateTopK-BoredInf-v0
+python /var/scratch/yal700/Master_thesis_RLRSs/pretrain_gems.py --slate-size $SLATESIZE --latent-dim $LD --n-users 100000 --device cuda --multi False --num-item 100 --seed 2023 --concurrent True --env-id SlateTopK-BoredInf-v0
 
 deactivate
