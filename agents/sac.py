15ef64ce156be8432221416bbdf2d776bb64d355
YasBenAll <yassinbenallal@hotmail.com>
2024-10-06 15:03:26 +0200

    Update sac.py

--- agents/sac.py
+++ agents/sac.py
@@ -367,6 +367,7 @@ class Actor(nn.Module):
             return action
 
 def train(args, decoder = None):
+    print(f"Training {args.env_id} with {args.algorithm} on {args.device}")
     run_name = f"{args.env_id}__{args.run_name}__{args.seed}__{int(time.time())}"
     if args.track == "wandb":
         import wandb
