9caa848b3e949bfa2ef49f55c7b323fac771aa3d
YasBenAll <yassinbenallal@hotmail.com>
2024-10-20 23:30:26 +0200

    Update train_hac.sh

--- train_hac.sh
+++ train_hac.sh
@@ -30,4 +30,4 @@ SEED=${betas[SLURM_ARRAY_TASK_ID]}
 
 
 # Run the actual experiment
-python /var/scratch/yal700/Master_thesis_RLRSs/train.py --agent hac --seed $SEED --total-timesteps 500000 --val-interval 5000 --policy-frequency 1 --observable True --buffer-size 100 --env-id sardine/SlateTopK-Bored-v0 --morl False --learning-starts 10000 --observable True  
+python /var/scratch/yal700/Master_thesis_RLRSs/train.py --agent hac --seed $SEED --total-timesteps 500000 --val-interval 5000 --policy-frequency 1 --observable True --buffer-size 100 --env-id sardine/SlateTopK-Bored-v0 --morl False --learning-starts 10000 --observable True --ml100k True
